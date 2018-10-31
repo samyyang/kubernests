159c4097d03bd600e0402501222406fcf039a445
yangw866 <yangw866@sina.com>
2018-11-01 00:52:18 +0800

    xx

--- microservice/message-thrift-python-service/build_base.sh
+++ microservice/message-thrift-python-service/build_base.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -t python-base:latest -f Dockerfile.base .
+docker tag python-base:latest 192.168.116.141:5000/micro-service/python-base:latest
+docker push 192.168.116.141:5000/micro-service/python-base:latest
